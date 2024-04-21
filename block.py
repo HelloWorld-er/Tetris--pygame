9f63d22e9783e16d2b2c54e5625e18fca855c9be
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2024-04-21 23:43:50 +0800

    Update block.py

--- block.py
+++ block.py
@@ -3,20 +3,28 @@ from pygame.sprite import Sprite
 
 
 class Block(Sprite):
-	"""For each block in a tetris"""
 	def __init__(self, config, screen, color, block_pos):
 		super(Block, self).__init__()
 		self.config = config
 		self.screen = screen
 		self.color = color
+		self.block_pos = block_pos
 		
 		self.rect = pygame.Rect(block_pos[0], block_pos[1], self.config.block_size, self.config.block_size)
 		self.rect.x = self.rect.width * block_pos[0]
 		self.rect.y = self.rect.height * block_pos[1]
+		
+		self.tetris = None
 	
-	def update_pos(self, origin_pos):
-		self.rect.x += origin_pos[0] * self.rect.width
-		self.rect.y += origin_pos[1] * self.rect.height
+	def add_to_group(self, tetris):
+		self.tetris = tetris
+		self.tetris.group.add(self)
+		self.color = self.tetris.color
+	
+	def remove_from_group(self):
+		self.color = self.config.bg_color
+		self.tetris.group.remove(self)
+		self.tetris = None
 	
 	def draw_block(self):
 		pygame.draw.rect(self.screen, self.color, self.rect)
