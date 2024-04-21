00111ed85c0d7422901023a9f5fe46776dc3d0e6
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2024-04-21 23:43:58 +0800

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -1,22 +1,35 @@
-from pygame.sprite import Sprite, Group
+from pygame.sprite import Group
 import random
 
 
-class Tetris(Sprite):
-	def __init__(self, config, screen):
+class Tetris():
+	def __init__(self, config, screen, blocks):
 		super(Tetris, self).__init__()
 		self.config = config
 		self.screen = screen
 		self.group = Group()
 		self.color = random.choice(self.config.tetris_colors)[1]
 		self.pattern = random.choice(self.config.tetris_patterns)
-		self.origin = [random.randint(0, int(self.config.screen_column - max([_[0] for _ in self.pattern]))), 0]
+		
+		self.origin = []
 	
-	def update_pos(self, origin_pos):
+		# self.origin = [random.randint(0, int(self.config.screen_column - max([_[0] for _ in self.pattern]))), 0]
+	
+	def update_pos(self, blocks, origin_pos):
 		self.origin[0] += origin_pos[0]
 		self.origin[1] += origin_pos[1]
+		
 		for block in self.group.sprites():
-			block.update_pos(self.origin)
+			block.remove_from_group()
+		
+		for block_relative_pos in self.pattern:
+			expected_pos = (self.origin[0] + block_relative_pos[0], self.origin[1] + block_relative_pos[1])
+			if expected_pos[0] < 0 or expected_pos[1] < 0:
+				continue
+			try:
+				blocks[expected_pos[1]][expected_pos[0]].add_to_group(self)
+			except IndexError:
+				pass
 	
 	def draw_tetris(self):
 		for block in self.group.sprites():
