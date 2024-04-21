318b150dd12ea98a835921ef3ba88842a72c3d58
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2024-04-21 21:45:29 +0800

    Update game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from tetris import Tetris
+from block import Block
 
 
 def check_keydown_events(event):
@@ -23,6 +24,25 @@ def check_events(stats, play_button):
 		elif event.type == pygame.MOUSEBUTTONDOWN:
 			mouse_x, mouse_y = pygame.mouse.get_pos()
 			check_button(stats, play_button, mouse_x, mouse_y)
+		elif event.type == pygame.USEREVENT + 1:  # == tetris_auto_down_event
+			# tetris_down
+			if stats.tetris_controlling:
+				pygame.time.set_timer(pygame.USEREVENT + 1, 1000)
+
+
+def create_new_tetris(config, screen, stats, tetris_group):
+	tetris = Tetris(config, screen)
+	for block_pos in tetris.pattern:
+		tetris.group.add(Block(tetris.config, tetris.screen, tetris.color, block_pos))
+	tetris.update_pos((0, 0))
+	tetris_group.add(tetris)
+	
+	stats.tetris_controlling = True
+	
+
+def update_tetris_group(tetris_group):
+	for tetris in tetris_group.sprites():
+		tetris.draw_tetris()
 
 
 def draw_grid(config, screen):
@@ -32,11 +52,14 @@ def draw_grid(config, screen):
 		pygame.draw.line(screen, config.grid_color, (0,y), (config.screen_width, y))
 
 
-def update_screen(config, screen, stats, play_button):
+def update_screen(config, screen, stats, tetris_group, play_button):
 	screen.fill(config.bg_color)
-	draw_grid(config, screen)
 	
 	if stats.game_active is False:
+		draw_grid(config, screen)
 		play_button.draw_button()
+	else:
+		update_tetris_group(tetris_group)
+		draw_grid(config, screen)
 	
 	pygame.display.flip()
