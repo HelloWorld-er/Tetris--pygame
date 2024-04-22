794d54085dcb301ed79e0e148de2e03a83974d1c
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2024-04-22 23:01:00 +0800

    Update block.py

--- block.py
+++ block.py
@@ -8,11 +8,12 @@ class Block(Sprite):
 		self.config = config
 		self.screen = screen
 		self.color = color
-		self.block_pos = block_pos
+		self.block_pos_column = block_pos[0]
+		self.block_pos_row = block_pos[1]
 		
-		self.rect = pygame.Rect(block_pos[0], block_pos[1], self.config.block_size, self.config.block_size)
-		self.rect.x = self.rect.width * block_pos[0]
-		self.rect.y = self.rect.height * block_pos[1]
+		self.rect = pygame.Rect(self.block_pos_column, self.block_pos_row, self.config.block_size, self.config.block_size)
+		self.rect.x = self.rect.width * self.block_pos_column
+		self.rect.y = self.rect.height * self.block_pos_row
 		
 		self.tetris = None
 	
@@ -21,10 +22,14 @@ class Block(Sprite):
 		self.tetris.group.add(self)
 		self.color = self.tetris.color
 	
-	def remove_from_group(self):
-		self.color = self.config.bg_color
+	def remove_from_group(self, save_color):
+		if not save_color:
+			self.color = self.config.bg_color
 		self.tetris.group.remove(self)
 		self.tetris = None
+	
+	def in_group(self, tetris):
+		return self.tetris == tetris
 	
 	def draw_block(self):
 		pygame.draw.rect(self.screen, self.color, self.rect)
