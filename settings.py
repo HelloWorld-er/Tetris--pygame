1c741820df4a9c828b93ac312df8ea36c4e63657
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2024-04-22 23:01:07 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -31,4 +31,5 @@ class Settings():
 			((0, 0), (1, 0), (0, -1), (1, -1), (0, -2), (1, -2))
 		]
 		
-		self.block_moving_speed = 50
+		self.tetris_auto_moving_speed = 50
+		self.tetris_manual_moving_speed = 40
