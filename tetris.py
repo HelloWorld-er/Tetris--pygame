a2a41f3c1435d8b222c053265d5f230f04d13a5f
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2024-04-22 08:07:17 +0800

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -3,13 +3,15 @@ import random
 
 
 class Tetris():
-	def __init__(self, config, screen, blocks):
+	def __init__(self, config, screen):
 		super(Tetris, self).__init__()
 		self.config = config
 		self.screen = screen
-		self.group = Group()
-		self.color = random.choice(self.config.tetris_colors)[1]
-		self.pattern = random.choice(self.config.tetris_patterns)
+		
+		self.group = None
+		
+		self.color = None
+		self.pattern = None
 		
 		self.moving_left = False
 		self.moving_right = False
@@ -18,6 +20,13 @@ class Tetris():
 		self.origin = []
 	
 		# self.origin = [random.randint(0, int(self.config.screen_column - max([_[0] for _ in self.pattern]))), 0]
+	def initialize_tetris(self, blocks):
+		self.group = Group()
+		self.color = random.choice(self.config.tetris_colors)[1]
+		self.pattern = random.choice(self.config.tetris_patterns)
+		self.origin = [
+			random.randint(0, int(self.config.screen_column - max([_[0] for _ in self.pattern])) - 1), 0]
+		self.update_pos(blocks, (0, 0))
 	
 	def update_pos(self, blocks, origin_pos):
 		self.origin[0] += origin_pos[0]
