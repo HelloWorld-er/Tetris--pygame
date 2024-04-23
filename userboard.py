0a7e46e243910fd19422d670fa1e0ebd93bf6196
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2024-04-23 22:25:08 +0800

    Create userboard.py

---
+++ userboard.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+
+
+class UserBoard():
+	def __init__(self, root, config, stats):
+		self.config = config
+		self.stats = stats
+		
+		self.window = tk.Frame(root, borderwidth=1, relief="solid")
+		
+		self.username = ""
+		
+		self.username_entry_hint = tk.Label(self.window, text="Username: ")
+		self.username_entry = tk.Entry(self.window)
+		self.username_commit_button = tk.Button(self.window, text="Commit", command=self.retrieve_input)
+		
+		self.username_empty_hint = tk.Label(self.window, text="Username cannot be empty!", fg="red")
+	
+	def retrieve_input(self):
+		input_value = self.username_entry.get()
+		if input_value:
+			self.username = input_value
+			self.stats.login = True
+			if self.username_empty_hint.winfo_ismapped():
+				self.username_empty_hint.pack_forget()
+		else:
+			self.username_empty_hint.pack(expand=True, side="bottom")
+		
+	
+	def place_user_board(self):
+		self.window.place(relx=self.config.play_screen_width / self.config.screen_width,
+		                  rely=self.config.scoreboard_height / self.config.screen_height,
+		                  relwidth=self.config.userboard_width / self.config.screen_width,
+		                  relheight=self.config.userboard_height / self.config.screen_height)
+		self.username_entry_hint.pack(expand=True, side='left')
+		self.username_entry.pack(expand=True, side='left')
+		self.username_commit_button.pack(expand=True, side='right')
