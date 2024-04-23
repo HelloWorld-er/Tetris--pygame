9d769e4e21f4a13beddc94265f91cfcb06af8627
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2024-04-23 22:25:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
-import os
 import tkinter as tk
+from tkinter import font
 
 import tetris_game
 from settings import Settings
+from game_stats import GameStats
+from scoreboard import ScoreBoard
+from userboard import UserBoard
 
 
 def run_program():
 	root = tk.Tk()
 	root.title("Tetris")
+	
 	config = Settings()
-	# root.geometry(f"{config.screen_width}x{config.screen_height}+{int((root.winfo_screenwidth()-config.screen_width)/2)}+{int((root.winfo_screenheight()-config.screen_height)/2)}")
-	root.geometry(f"{config.screen_width}x{config.screen_height}")
-	# game_window = tk.Frame(root, width=config.play_screen_width, height=config.play_screen_height)
-	# game_window.grid(row=0, column=0)
+	stats = GameStats()
 	
-	# os.environ["SDL_WINDOWID"] = str(game_window.winfo_id())
+	root.geometry(
+		f"{config.screen_width}x{config.screen_height}+{int((root.winfo_screenwidth() - config.screen_width) / 2)}+0")
+	game_window = tk.Frame(root, width=config.play_screen_width, height=config.play_screen_height, borderwidth=1, relief="solid")
+	game_window.place(relx=0, rely=0)
 	
-	# label = tk.Label(root, text="hello")
-	# label.grid(row=0, column=1)
+	game_window_button = tk.Button(game_window, text="Game Window", font=font.Font(size=40))
+	game_window_button.place(relwidth=1, relheight=1, relx=0.5, rely=0.5, anchor='center')
+	game_window_button.config(command=lambda: tetris_game.run_game(config, stats, scoreboard_window, user_board_window, int((root.winfo_screenwidth() - config.screen_width)/2), 0))
+	
+	scoreboard_window = ScoreBoard(root, config, stats)
+	scoreboard_window.place_scoreboard()
+	
+	user_board_window = UserBoard(root, config, stats)
+	user_board_window.place_user_board()
 	
 	root.mainloop()
-	
-	# tetris_game.run_game(config)
+
+
+# def login():
 
 
 run_program()
