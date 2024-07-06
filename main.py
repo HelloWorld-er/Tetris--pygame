ad8770565434c03429b5a1cb2ac6b22e76b88697
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2024-07-06 23:19:54 +0800

    Refactor whole project

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ class UISetting:
 	"""
 	Set the UI properties of a specific region.
 	
+	Methods:
+		__init__: initializes the object and the UI properties.
+	
+	Attributes:
+		screen_width: the width of the region the object is used for.
+		screen_height: the height of the region the object is used for.
+		bg_color: the background color of the UI object.
+	
 	"""
 	def __init__(self, screen_width=1200, screen_height=800, screen_caption="", bg_color=(255, 255, 255)):
 		self.screen_width = screen_width
